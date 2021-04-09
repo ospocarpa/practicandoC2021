619279c2037d2593e46ce4a3826b5c37b35c6a30
Oscar Hinojosa <oscarhinnojosa@gmail.com>
2021-04-09 02:24:45 -0300

    suma

--- practicaC/src/practicaC.c
+++ practicaC/src/practicaC.c
@@ -2,21 +2,18 @@
 
 
 int main(){
-    // t_log* logger = log_create("./cfg/practicaC.log", "PRACTICAC", true, LOG_LEVEL_INFO);
-    // log_info(logger, "Soy una practicaC! %s", mi_funcion_compartida());
-    // log_destroy(logger);
+    // Lee dos valores enteros e informar su suma
 
     
-    char nombre[20] ;
-    int edad ;
-    double altura ;
-    printf("Ingrese su nombre\n");
-    scanf("%s",nombre);
-     printf("Ingrese su edad\n");
-    scanf("%d",&edad);
-     printf("Ingrese su altura\n");
-    scanf("%lf",&altura);
-    printf("Ud  es %s tiene %d anios y una altura de %.2lf metros\n",nombre,edad,altura);
+   
+    int n1,n2 ;
+    
+    printf("Ingrese un entero\n");
+    scanf("%d",&n1);
+    printf("Ingrese un entero\n");
+    scanf("%d",&n2);
+    
+    printf("%d + %d = %d\n",n1,n2,n1+n2);
 
 
 }
