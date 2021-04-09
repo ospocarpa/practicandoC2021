c4b5b3490d0ed7f36ad81db83a49ef51da00ca6a
Oscar Hinojosa <oscarhinnojosa@gmail.com>
2021-04-09 16:52:44 -0300

    separFecha

--- practicaC/src/practicaC.c
+++ practicaC/src/practicaC.c
@@ -1,25 +1,18 @@
 #include "practicaC.h"
 
+int main()
+{
+    // pasar de aaaammdd a : aniio , mes, dia
+    long fec;
+    int dia, mes, anio;
+    printf("Ingrese una fecha(aaaaammdd)\n");
+    scanf("%ld", &fec);
+    anio = fec / 10000;
+    int res = fec % 10000;
+    mes = res / 100;
+    dia = res % 100;
 
-int main(){
-    // Lee dos valores enteros e informar su cociente
-
-    
-   
-    int n1,n2 ;
-    float div;
-    printf("Ingrese un entero\n");
-    scanf("%d",&n1);
-    printf("Ingrese un entero\n");
-    scanf("%d",&n2);
-    if(n2 != 0) {
-        div = (float)n1/n2;
-        printf("%d/%d=%.2lf\n",n1,n2,div);
-    }
-    else  {
-        printf("No se puede reaizar la division con dividendo igual a cero\n");
-    }
-    
-
-
+    printf("Dia:  %d\n", dia);
+    printf("Mes:  %d\n", mes);
+    printf("Anio: %d\n", anio);
 }
