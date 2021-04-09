f3c677e472afa5c8e20c5fdd8b1ef52532c477f0
Oscar Hinojosa <oscarhinnojosa@gmail.com>
2021-04-09 02:32:37 -0300

    division entera

--- practicaC/src/practicaC.c
+++ practicaC/src/practicaC.c
@@ -2,18 +2,24 @@
 
 
 int main(){
-    // Lee dos valores enteros e informar su suma
+    // Lee dos valores enteros e informar su cociente
 
     
    
     int n1,n2 ;
-    
+    float div;
     printf("Ingrese un entero\n");
     scanf("%d",&n1);
     printf("Ingrese un entero\n");
     scanf("%d",&n2);
+    if(n2 != 0) {
+        div = (float)n1/n2;
+        printf("%d/%d=%.2lf\n",n1,n2,div);
+    }
+    else  {
+        printf("No se puede reaizar la division con dividendo igual a cero\n");
+    }
     
-    printf("%d + %d = %d\n",n1,n2,n1+n2);
 
 
 }
