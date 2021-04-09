dfcc219f3acbe2bda3215006dcac53b7da4a5459
Oscar Hinojosa <oscarhinnojosa@gmail.com>
2021-04-09 02:21:18 -0300

    ingrese de datos

--- practicaC/src/practicaC.c
+++ practicaC/src/practicaC.c
@@ -5,12 +5,18 @@ int main(){
     // t_log* logger = log_create("./cfg/practicaC.log", "PRACTICAC", true, LOG_LEVEL_INFO);
     // log_info(logger, "Soy una practicaC! %s", mi_funcion_compartida());
     // log_destroy(logger);
+
     
-    
-    char nombre[] = "Oscar Hinojosa";
-    int edad = 26;
-    double altura =1.73;
-    printf("Mi nombre es %s tengo %d anios y mido %.2lf metros\n",nombre,edad,altura);
+    char nombre[20] ;
+    int edad ;
+    double altura ;
+    printf("Ingrese su nombre\n");
+    scanf("%s",nombre);
+     printf("Ingrese su edad\n");
+    scanf("%d",&edad);
+     printf("Ingrese su altura\n");
+    scanf("%lf",&altura);
+    printf("Ud  es %s tiene %d anios y una altura de %.2lf metros\n",nombre,edad,altura);
 
 
 }
