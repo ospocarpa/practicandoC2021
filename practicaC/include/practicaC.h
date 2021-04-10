44aaf6bd76808f052b2871986b7d37942501b1d6
Oscar Hinojosa <oscarhinnojosa@gmail.com>
2021-04-10 18:33:25 -0300

    n primos:muevo funcion

--- practicaC/include/practicaC.h
+++ practicaC/include/practicaC.h
@@ -6,4 +6,20 @@
 #include "shared_utils.h"
 #include "string.h"
 
+
+int esPrimo(int n)
+{
+    int esPrimo = n < 2 ? 0 : 1; // 0 = false
+    for (int i = 2; i < n && esPrimo; i++)
+    {
+        if (n % i == 0)
+        {
+            esPrimo = 0;
+        }
+    }
+    return esPrimo;
+}
+
+
+
 #endif
--- practicaC/src/practicaC.c
+++ practicaC/src/practicaC.c
@@ -1,17 +1,6 @@
 #include "practicaC.h"
 
-int esPrimo(int n)
-{
-    int esPrimo = n < 2 ? 0 : 1; // 0 = false
-    for (int i = 2; i < n && esPrimo; i++)
-    {
-        if (n % i == 0)
-        {
-            esPrimo = 0;
-        }
-    }
-    return esPrimo;
-}
+
 
 int main()
 {
