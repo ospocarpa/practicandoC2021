baa947f657d67d2a8dc36235b53d22e17ac1040f
Oscar Hinojosa <oscarhinnojosa@gmail.com>
2021-04-10 20:14:21 -0300

    ejercicio con enunciado

--- practicaC/include/practicaC.h
+++ practicaC/include/practicaC.h
@@ -6,6 +6,20 @@
 #include "shared_utils.h"
 #include "string.h"
 
+void dividirFecha(long f, int *d, int *m, int *a)
+{
+    *a = f / 10000;
+    int resto = f % 10000;
+    *m = resto / 100;
+    *d = resto % 100;
+}
 
+int esAnioBisiesto(int a)
+{
+    int div4 = (a % 4 == 0);
+    int div100 = (a % 100 == 0);
+    int div400 = (a % 400 == 0);
+    return div4 && (!div100 || div400);
+}
 
 #endif
--- practicaC/src/practicaC.c
+++ practicaC/src/practicaC.c
@@ -1,18 +1,43 @@
 #include "practicaC.h"
 
-void permutar(int *a, int *b)
-{
-    int aux = *a;
-    *a = *b;
-    *b = aux;
-}
-
 int main()
 {
-    // Permutar (funcion pasando direccion de memoria de los argumentos)
+    // Se dispone de un cjto de valores que representan fechas como numeros  enteros de 8 digitos (aaaammdd)
+    //Informar
+    //1)    cuantas fechas son de marzo
+    // 2)   cuantas fechas tienen anios bisiestos
+    // 3)   errores : con m=2,d=29 y anio no bisiesto
 
-    int x = 5, y = 2;
-    printf("Antes de permutar:x=%d\ty=%d\n", x, y);
-    permutar(&x, &y);
-    printf("Despues de permutar:x=%d\ty=%d\n", x, y);
+    int cantMarzo = 0, cantBisiestos = 0, cantErrores = 0;
+    long f;
+    int dia, mes, anio;
+    printf("Ingrese una fecha (0 para finalizar)\n");
+    scanf("%ld", &f);
+    while (f != 0)
+    {
+        dividirFecha(f, &dia, &mes, &anio);
+        int anioBisiesto = esAnioBisiesto(anio);
+        if (mes == 3)
+        {
+            cantMarzo++;
+        }
+
+        if (anioBisiesto)
+        {
+            cantBisiestos++;
+        }
+        else if (mes == 2 && dia == 29)
+        {
+            cantErrores++;
+        }
+
+        printf("-----------------------------------------\n");
+        printf("Ingrese una fecha (0 para finalizar)\n");
+        scanf("%ld", &f);
+    }
+    printf("Fechas de marzo:%d\n", cantMarzo);
+    printf("Anios bisiestos:%d\n", cantBisiestos);
+    printf("Fechas con error:%d\n", cantErrores);
+
+    return 0;
 }
