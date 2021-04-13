73f94fc4a6b65ec2a922f4969953bc8a34c713bb
Oscar Hinojosa <oscarhinnojosa@gmail.com>
2021-04-13 16:39:16 -0300

    array dinamico char*

--- practicaC/src/practicaC.c
+++ practicaC/src/practicaC.c
@@ -2,24 +2,26 @@
 
 int main()
 {
-    // creando arrays dinamicamente
+    // creando arrays dinamicamente de tipo char*
     int n;
     printf("Ingrese un entero para dimensionar un array\n");
     scanf("%d", &n);
-    int *sArr;
-    sArr = (int *)malloc(sizeof(int) * n);
+    char *h;
+
+    char *sArr = (char *)malloc(n);
 
     // inicializamos los valores
+    scanf("%s", h);
     for (int i = 0; i < n; i++)
     {
-        printf("Numero: ");
-        scanf("%d", &sArr[i]);
+
+        sArr[i] = h[i];
     }
     // sArr[n] = '\0';
     // mostramos los valores
     for (int i = 0; i < n; i++)
     {
-        printf("%d ", sArr[i]);
+        printf("%c ", sArr[i]);
     }
     return 0;
 }
