e5df963ffa767583786bbc8ed3a5fe7f2c85f38c
Oscar Hinojosa <oscarhinnojosa@gmail.com>
2021-04-12 23:53:33 -0300

    aritmetica de direcciones

--- practicaC/src/practicaC.c
+++ practicaC/src/practicaC.c
@@ -4,7 +4,10 @@ int main()
 {
 
     char s[] = "Esto es una cadena";
-    recibeArray(s);
-    recibePuntero(s);
+    printf("[%s]\n", s);
+    printf("[%s]\n", s + 5);
+    printf("[%s]\n", s + 7);
+    printf("[%s]\n", s + 9);
+    printf("[%s]\n", s + 12);
     return 0;
 }
