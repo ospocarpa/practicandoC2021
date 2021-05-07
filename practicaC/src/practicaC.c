07182b7e1ce7d3113e641e6eed234b9495d5c636
Oscar Hinojosa <oscarhinnojosa@gmail.com>
2021-05-07 20:22:40 -0300

    Ejemplo puntero declaro p como un puntero a un entero ,reservo su memoria y alcontenido este le asigno 1. despues muestro por consola el valor entero el puntero y la direccion de memoria del puntero

--- practicaC/src/practicaC.c
+++ practicaC/src/practicaC.c
@@ -1,28 +1,13 @@
 #include "practicaC.h"
 
-int main()
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
 {
-
-    //Dado un cjto de numeros enteros determinar cuantas veces se repite cada uno
-    // los datos se ingresan sin orden y finaliza con 0 .Se garantiza que a lo sumo habra 100 numeros diferentes
-    int arr[50], aux[50];
-    int len = 0;
-    int enc;
-    int n;
-    printf("Ingrese un numero\n");
-    scanf("%d", &n);
-    while (len < 100 && n != 0)
-    {
-        int pos = buscarYAgregar(aux, &len, n, &enc);
-        arr[pos] = enc ? ++arr[pos] : 1;
-        printf("Ingrese un numero\n");
-        scanf("%d", &n);
-    }
-    printf("______________________\n");
-    for (int i = 0; i < len; i++)
-    {
-        printf("%d aparece %d veces\n", aux[i], arr[i]);
-    }
-
+    int *p = malloc(sizeof(int));
+    *p = 1;
+    printf("p = %d\n", p);
+    printf("&p = %p\n", &p);
     return 0;
 }
