9f95ea12f93c3daf4844e1e06aa2f42f66df17f6
Oscar Hinojosa <oscarhinnojosa@gmail.com>
2021-05-07 21:16:46 -0300

    Desreferencia Cambio el valor de i=1 por punteros

--- practicaC/src/practicaC.c
+++ practicaC/src/practicaC.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//desrefenrencia :cambia el valor de i=1 a i=2 por punteros
+//mostrar valores antes y despues
 int main(void)
 {
+    int i = 4;
     int *p = malloc(sizeof(int));
-    *p = 1;
-    printf("p = %d\n", p);
-    printf("&p = %p\n", &p);
-    return 0;
-}
+    p = &i;
+    printf("Antes i vale:%d\n", i);
