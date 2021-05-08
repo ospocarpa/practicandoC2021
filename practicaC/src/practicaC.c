b5a38cbc3929ec5966f3918dd51b545408e9325f
Oscar Hinojosa <oscarhinnojosa@gmail.com>
2021-05-07 21:28:16 -0300

    usando la shared

--- practicaC/src/practicaC.c
+++ practicaC/src/practicaC.c
@@ -2,6 +2,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <commons/log.h>
+#include <commons/string.h>
 
 //desrefenrencia :cambia el valor de i=1 a i=2 por punteros
 //mostrar valores antes y despues
@@ -11,3 +13,10 @@ int main(void)
     int *p = malloc(sizeof(int));
     p = &i;
     printf("Antes i vale:%d\n", i);
+    *p = 2;
+    printf("Ahora i vale:%d\n", *p);
+    t_log *logger = log_create("cfg/practicaC.log", "PRACTICAC", 1, LOG_LEVEL_INFO);
+    log_info(logger, "Usando la shared\n");
+    log_destroy(logger);
+    //free(p);
+}
