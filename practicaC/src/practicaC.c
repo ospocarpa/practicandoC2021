dc262f47debb36bb4701dbecf349167927d86666
Oscar Hinojosa <oscarhinnojosa@gmail.com>
2021-05-07 22:18:40 -0300

    Solucionado error de desreferencia Ahora lupi conoce a su hijo :)

--- practicaC/src/practicaC.c
+++ practicaC/src/practicaC.c
@@ -10,7 +10,7 @@ typedef struct
     char nombre[20];
     char apellido[20];
     int edad;
-    t_persona *hijo;
+    struct t_persona *hijo;
 } t_persona;
 int main(void)
 {
@@ -18,6 +18,19 @@ int main(void)
     strcpy(p->nombre, "Esteban");
     strcpy(p->apellido, "Lupi");
     p->edad = 26;
-    p->hijo = NULL;
+    p->hijo = malloc(sizeof(t_persona));
     printf("\nNombre:%s\nApellido:%s\nEdad:%d\n", p->nombre, p->apellido, p->edad);
+    printf("Creamos al hijo de esteban\n");
+    printf("_---------------\n");
+    t_persona *nene = malloc(sizeof(t_persona));
+    strcpy(nene->nombre, "nico");
+    strcpy(nene->apellido, "Lupi");
+    nene->edad = 1;
+    nene->hijo = NULL;
+    p->hijo = nene;
+    printf("_---------------\n");
+    printf("\nNombre:%s\nApellido:%s\nEdad:%d\nHijo:%p", p->nombre, p->apellido, p->edad, p->hijo);
+    printf("_---------------\n");
+    printf("\nnombre:%s\nApellido:%s\nEdad:%d\nHijo:%p", nene->nombre, nene->apellido, nene->edad, nene->hijo);
+    printf("\nLa direccion de nene es %p\n", nene);
 }
